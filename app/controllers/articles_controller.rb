1ec10e02c49f4b4caf6e069a4631d7e70d7c9802
Ubuntu <ubuntu@ip-172-31-24-241.us-east-2.compute.internal>
2024-04-15 01:47:55 +0000

    finished project

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "jps", password: "kenyon", except: [ :index, :show]
+
   def index
      @articles=Article.all
   end
